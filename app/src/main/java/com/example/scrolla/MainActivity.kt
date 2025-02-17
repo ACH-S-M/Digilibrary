7ad1314881e9182d0a9bb3fcf850fdc35f70d275
ACH-S-M <MuhamadihsanM101@gmail.com>
2025-02-17 14:38:07 +0700

    Navdrawer Jadi with Route( Belum Bikin Page baru buat Menu drawer)

--- app/src/main/java/com/example/scrolla/MainActivity.kt
+++ app/src/main/java/com/example/scrolla/MainActivity.kt
@@ -1,28 +1,33 @@
 package com.example.scrolla
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
@@ -34,13 +39,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.scrolla.ui.theme.ScrollaTheme
+import com.example.scrolla.ui.theme.colorPrimaryDigily
+import com.example.scrolla.ui.theme.digilysecondary
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,6 +74,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+    val context = LocalContext.current
     val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     DoubleBackExit(navController = navController)
@@ -68,11 +82,77 @@ fun MainScreen() {
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "Ini ada Dimna")
+                Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(intrinsicSize = IntrinsicSize.Max)){
+                 Column(modifier = Modifier
+                     .fillMaxWidth()
+                     .height(intrinsicSize = IntrinsicSize.Max)
+                     .padding(start = 40.dp, top = 60.dp), verticalArrangement = Arrangement.Center) {
+                     Image(painter = painterResource(id = R.drawable.elaina),
+                         contentDescription = "Profile",
+                         modifier = Modifier
+                             .size(150.dp)
+                             .clip(RoundedCornerShape(90.dp)),
+                         contentScale = ContentScale.FillWidth
+                     )
+                     Text(
+                         text = "Achaia.S.M",
+                         color = colorPrimaryDigily,
+                         modifier = Modifier.padding(top = 12.dp),
+                         fontSize = 28.sp,
+                         fontWeight = FontWeight.Bold,
+                     )
+                 }
+
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp)
+                        .clickable {
+                            Toast
+                                .makeText(context, "Edit Profil terbuka", Toast.LENGTH_SHORT)
+                                .show()
+                        },
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ){
+                        Icon(imageVector = Icons.Outlined.Edit,
+                            contentDescription = "Edit_Profile",modifier = Modifier.padding(start = 30.dp) )
+                        Text(text = "Edit Profile", fontSize = 18.sp)
+
+                    }
+                    Divider(thickness = 2.dp, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp), color = colorPrimaryDigily)
                 }
+
+                // Batas atas
+                //Nav Menu
+                Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(intrinsicSize = IntrinsicSize.Max)) {
+                    //Ini MASING MASING ROW BUAT KE MENU LAIN
+                    DrawerMenuItem(icon = Icons.Filled.Send, text = "Kirim Novel") {
+                        navController.navigate("drawer"){popUpTo(0)}
+                        scope.launch { drawerState.close() }
+                    }
+                    DrawerMenuItem(icon = Icons.Filled.ThumbUp, text = "Populer") {
+                        navController.navigate("drawer"){popUpTo(0)}
+                        scope.launch { drawerState.close() }
+                    }
+
+                }
+                //Setting Akun
+                Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
+                    Divider(thickness = 2.dp, modifier = Modifier.fillMaxWidth(), color = colorPrimaryDigily)
+                    DrawerMenuItem(icon = Icons.Filled.Settings, text = "Setting") {
+                        navController.navigate("favorites")
+                    }
+                }
+
             }
-        }) {
+        },
+    ) {
         Scaffold(
             topBar = { TopMenuBar(drawerState = drawerState, scope = scope)},
             bottomBar = { BottomNavbar(navController) }
--- app/src/main/java/com/example/scrolla/NavigationHost.kt
+++ app/src/main/java/com/example/scrolla/NavigationHost.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -26,10 +27,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -38,6 +42,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.scrolla.ui.theme.ContentDigily
 import com.example.scrolla.ui.theme.Favorite
 import com.example.scrolla.ui.theme.ScrollaTheme
+import com.example.scrolla.ui.theme.colorPrimaryDigily
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -59,21 +64,25 @@ fun BottomNavbar(navController: NavController,modifier:Modifier = Modifier) {
                Column(
                    modifier
                        .width(IntrinsicSize.Max)
-                       .clickable { navController.navigate("home"){popUpTo(0)} }, horizontalAlignment = Alignment.CenterHorizontally) {
+                       .clickable { navController.navigate("home") { popUpTo(0) } }, horizontalAlignment = Alignment.CenterHorizontally) {
                    Icon(imageVector = Icons.Filled.Home,
                        contentDescription = "Home",
                        modifier.size(30.dp))
                    Text(text = "Home")
                }
-                Column(modifier.width(IntrinsicSize.Max)
-                    .clickable { navController.navigate("search"){popUpTo(0)} }, horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    modifier
+                        .width(IntrinsicSize.Max)
+                        .clickable { navController.navigate("search") { popUpTo(0) } }, horizontalAlignment = Alignment.CenterHorizontally) {
                     Icon(imageVector = Icons.Filled.Search,
                         contentDescription = "Search",
                         modifier.size(30.dp))
                     Text(text = "Search")
                 }
-                Column(modifier.width(IntrinsicSize.Max)
-                    .clickable { navController.navigate("favorites"){popUpTo(0)} }, horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    modifier
+                        .width(IntrinsicSize.Max)
+                        .clickable { navController.navigate("favorites") { popUpTo(0) } }, horizontalAlignment = Alignment.CenterHorizontally) {
                     Icon(imageVector = Icons.Filled.Favorite,
                         contentDescription = "Favorite",
                         modifier.size(30.dp))
@@ -87,6 +96,7 @@ fun NavigationGraph(navController: NavHostController,modifier: Modifier = Modifi
         composable("home") { Home(navController) }
         composable("search") { ContentDigily(navController) }
         composable("favorites") { Favorite(navController) }
+        composable("drawer"){ ContentDigily(navController)}
     }
 }
 
@@ -105,6 +115,35 @@ fun DoubleBackExit(navController: NavController) {
                 backPressedOnce = false
             }
         }
+    }
+}
+// drawer Menu Item
+@Composable
+fun DrawerMenuItem(
+    icon: ImageVector,
+    text: String,
+    onClick: () -> Unit
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .padding(top=8.dp)
+            .clickable { onClick() }
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = text,
+            modifier = Modifier.padding(start = 30.dp).size(30.dp)
+        )
+        Text(
+            text = text,
+            color = colorPrimaryDigily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp
+        )
     }
 }
 @Preview(showBackground = true)
